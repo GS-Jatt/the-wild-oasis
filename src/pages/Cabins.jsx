a2caf0fc277d86b0657a5e7d654acaf41d618c9a
GS-Jatt <gurneetsinghjatt@gmail.com>
2023-08-21 16:13:49 +0530

    add

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,17 +2,17 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import { useQueryClient } from "@tanstack/react-query";
+import CabinTable from "../features/cabins/CabinTable";
+
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
+ 
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
-      <img src="https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
+      <CabinTable  ></CabinTable>
     </Row>
   );
 }
