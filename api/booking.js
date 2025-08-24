f4b874eed9c153ba882c11dc24e78b36fdf995a4
GS-Jatt <gurneetsinghjatt@gmail.com>
2025-08-24 12:08:22 +0530

    feat: add con job to update booking

---
+++ api/booking.js
@@ -0,0 +1,71 @@
+import { bookings } from "../src/data/data-bookings";
+import { cabins } from "../src/data/data-cabins";
+import supabase from "../src/services/supabase";
+import { subtractDates } from "../src/utils/helpers";
+import { isFuture, isPast, isToday } from "date-fns";
+
+export default async function handler(req, res) {
+  const { error } = await supabase.from("bookings").delete().gt("id", 0);
+  if (error) {
+    res.status(404).end(error.message);
+  }
+
+  const { data: guestsIds } = await supabase
+    .from("guests")
+    .select("id")
+    .order("id");
+  const allGuestIds = guestsIds.map((cabin) => cabin.id);
+  const { data: cabinsIds } = await supabase
+    .from("cabins")
+    .select("id")
+    .order("id");
+  const allCabinIds = cabinsIds.map((cabin) => cabin.id);
+
+  const finalBookings = bookings.map((booking) => {
+    // Here relying on the order of cabins, as they don't have and ID yet
+    const cabin = cabins.at(booking.cabinId - 1);
+    const numNights = subtractDates(booking.endDate, booking.startDate);
+    const cabinPrice = numNights * (cabin.regularPrice - cabin.discount);
+    const extrasPrice = booking.hasBreakfast
+      ? numNights * 15 * booking.numGuests
+      : 0; // hardcoded breakfast price
+    const totalPrice = cabinPrice + extrasPrice;
+
+    let status;
+    if (
+      isPast(new Date(booking.endDate)) &&
+      !isToday(new Date(booking.endDate))
+    )
+      status = "checked-out";
+    if (
+      isFuture(new Date(booking.startDate)) ||
+      isToday(new Date(booking.startDate))
+    )
+      status = "unconfirmed";
+    if (
+      (isFuture(new Date(booking.endDate)) ||
+        isToday(new Date(booking.endDate))) &&
+      isPast(new Date(booking.startDate)) &&
+      !isToday(new Date(booking.startDate))
+    )
+      status = "checked-in";
+
+    return {
+      ...booking,
+      numNights,
+      cabinPrice,
+      extrasPrice,
+      totalPrice,
+      guestId: allGuestIds.at(booking.guestId - 1),
+      cabinId: allCabinIds.at(booking.cabinId - 1),
+      status,
+    };
+  });
+
+  console.log(finalBookings);
+
+  const { error: err } = await supabase.from("bookings").insert(finalBookings);
+  if (err) res.status(404).end(error.message);
+
+  res.status(200).end("Hello Cron!");
+}
