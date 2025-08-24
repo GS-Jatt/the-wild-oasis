6e9b7820bb93665b01f7b56f499539071fc2122a
GS-Jatt <gurneetsinghjatt@gmail.com>
2025-08-24 13:19:08 +0530

    fix: fix api error

--- api/booking.js
+++ api/booking.js
@@ -1,10 +1,21 @@
-import { bookings } from "../src/data/data-bookings";
-import { cabins } from "../src/data/data-cabins";
-import supabase from "../src/services/supabase";
-import { subtractDates } from "../src/utils/helpers";
-import { isFuture, isPast, isToday } from "date-fns";
+import {
+  isFuture,
+  isPast,
+  isToday,
+  add,
+  parseISO,
+  differenceInDays,
+} from "date-fns";
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.VITE_S_URL;
+const supabaseKey = process.env.server_key;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
+  console.log("booking start");
+
   const { error } = await supabase.from("bookings").delete().gt("id", 0);
   if (error) {
     res.status(404).end(error.message);
@@ -62,10 +73,384 @@ export default async function handler(req, res) {
     };
   });
 
-  console.log(finalBookings);
+  console.log("bookings complete");
 
   const { error: err } = await supabase.from("bookings").insert(finalBookings);
-  if (err) res.status(404).end(error.message);
+  if (err) {
+    res.status(404).end(err.message);
+  }
 
   res.status(200).end("Hello Cron!");
 }
+
+function fromToday(numDays, withTime = false) {
+  const date = add(new Date(), { days: numDays });
+  if (!withTime) date.setUTCHours(0, 0, 0, 0);
+  return date.toISOString().slice(0, -1);
+}
+
+const bookings = [
+  // CABIN 001
+  {
+    created_at: fromToday(-20, true),
+    startDate: fromToday(0),
+    endDate: fromToday(7),
+    cabinId: 1,
+    guestId: 2,
+    hasBreakfast: true,
+    observations:
+      "I have a gluten allergy and would like to request a gluten-free breakfast.",
+    isPaid: false,
+    numGuests: 1,
+  },
+  {
+    created_at: fromToday(-33, true),
+    startDate: fromToday(-23),
+    endDate: fromToday(-13),
+    cabinId: 1,
+    guestId: 3,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 2,
+  },
+  {
+    created_at: fromToday(-27, true),
+    startDate: fromToday(12),
+    endDate: fromToday(18),
+    cabinId: 1,
+    guestId: 4,
+    hasBreakfast: false,
+    observations: "",
+    isPaid: false,
+    numGuests: 2,
+  },
+
+  // CABIN 002
+  {
+    created_at: fromToday(-45, true),
+    startDate: fromToday(-45),
+    endDate: fromToday(-29),
+    cabinId: 2,
+    guestId: 5,
+    hasBreakfast: false,
+    observations: "",
+    isPaid: true,
+    numGuests: 2,
+  },
+  {
+    created_at: fromToday(-2, true),
+    startDate: fromToday(15),
+    endDate: fromToday(18),
+    cabinId: 2,
+    guestId: 6,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 2,
+  },
+  {
+    created_at: fromToday(-5, true),
+    startDate: fromToday(33),
+    endDate: fromToday(48),
+    cabinId: 2,
+    guestId: 7,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: false,
+    numGuests: 2,
+  },
+
+  // CABIN 003
+  {
+    created_at: fromToday(-65, true),
+    startDate: fromToday(-25),
+    endDate: fromToday(-20),
+    cabinId: 3,
+    guestId: 8,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 4,
+  },
+  {
+    created_at: fromToday(-2, true),
+    startDate: fromToday(-2),
+    endDate: fromToday(0),
+    cabinId: 3,
+    guestId: 9,
+    hasBreakfast: false,
+    observations: "We will be bringing our small dog with us",
+    isPaid: true,
+    numGuests: 3,
+  },
+  {
+    created_at: fromToday(-14, true),
+    startDate: fromToday(-14),
+    endDate: fromToday(-11),
+    cabinId: 3,
+    guestId: 10,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 4,
+  },
+
+  // CABIN 004
+  {
+    created_at: fromToday(-30, true),
+    startDate: fromToday(-4),
+    endDate: fromToday(8),
+    cabinId: 4,
+    guestId: 11,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 4,
+  },
+  {
+    created_at: fromToday(-1, true),
+    startDate: fromToday(12),
+    endDate: fromToday(17),
+    cabinId: 4,
+    guestId: 12,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: false,
+    numGuests: 4,
+  },
+  {
+    created_at: fromToday(-3, true),
+    startDate: fromToday(18),
+    endDate: fromToday(19),
+    cabinId: 4,
+    guestId: 13,
+    hasBreakfast: false,
+    observations: "",
+    isPaid: true,
+    numGuests: 1,
+  },
+
+  // CABIN 005
+  {
+    created_at: fromToday(0, true),
+    startDate: fromToday(14),
+    endDate: fromToday(21),
+    cabinId: 5,
+    guestId: 14,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: false,
+    numGuests: 5,
+  },
+  {
+    created_at: fromToday(-6, true),
+    startDate: fromToday(-6),
+    endDate: fromToday(-4),
+    cabinId: 5,
+    guestId: 15,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 4,
+  },
+  {
+    created_at: fromToday(-4, true),
+    startDate: fromToday(-4),
+    endDate: fromToday(-1),
+    cabinId: 5,
+    guestId: 16,
+    hasBreakfast: false,
+    observations: "",
+    isPaid: true,
+    numGuests: 6,
+  },
+
+  // CABIN 006
+  {
+    created_at: fromToday(-3, true),
+    startDate: fromToday(0),
+    endDate: fromToday(11),
+    cabinId: 6,
+    guestId: 17,
+    hasBreakfast: false,
+    observations:
+      "We will be checking in late, around midnight. Hope that's okay :)",
+    isPaid: true,
+    numGuests: 6,
+  },
+  {
+    created_at: fromToday(-16, true),
+    startDate: fromToday(-16),
+    endDate: fromToday(-9),
+    cabinId: 6,
+    guestId: 18,
+    hasBreakfast: true,
+    observations: "I will need a rollaway bed for one of the guests",
+    isPaid: true,
+    numGuests: 4,
+  },
+  {
+    created_at: fromToday(-18, true),
+    startDate: fromToday(-4),
+    endDate: fromToday(-1),
+    cabinId: 6,
+    guestId: 19,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 6,
+  },
+
+  // CABIN 007
+  {
+    created_at: fromToday(-2, true),
+    startDate: fromToday(17),
+    endDate: fromToday(23),
+    cabinId: 7,
+    guestId: 20,
+    hasBreakfast: false,
+    observations: "",
+    isPaid: false,
+    numGuests: 8,
+  },
+  {
+    created_at: fromToday(-7, true),
+    startDate: fromToday(40),
+    endDate: fromToday(50),
+    cabinId: 7,
+    guestId: 21,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 7,
+  },
+  {
+    created_at: fromToday(-55, true),
+    startDate: fromToday(32),
+    endDate: fromToday(37),
+    cabinId: 7,
+    guestId: 22,
+    hasBreakfast: true,
+    observations: "",
+    isPaid: true,
+    numGuests: 6,
+  },
+
+  // CABIN 008
+  {
+    created_at: fromToday(-8, true),
+    startDate: fromToday(-5),
+    endDate: fromToday(0),
+    cabinId: 8,
+    guestId: 1,
+    hasBreakfast: true,
+    observations:
+      "My wife has a gluten allergy so I would like to request a gluten-free breakfast if possible",
+    isPaid: true,
+    numGuests: 9,
+  },
+  {
+    created_at: fromToday(0, true),
+    startDate: fromToday(0),
+    endDate: fromToday(5),
+    cabinId: 8,
+    guestId: 23,
+    hasBreakfast: true,
+    observations:
+      "I am celebrating my anniversary, can you arrange for any special amenities or decorations?",
+    isPaid: true,
+    numGuests: 10,
+  },
+  {
+    created_at: fromToday(-10, true),
+    startDate: fromToday(10),
+    endDate: fromToday(13),
+    cabinId: 8,
+    guestId: 24,
+    hasBreakfast: false,
+    observations: "",
+    isPaid: true,
+    numGuests: 7,
+  },
+];
+
+const imageUrl = `${supabaseUrl}/storage/v1/object/public/cabin-images/`;
+
+const cabins = [
+  {
+    name: "001",
+    maxCapacity: 2,
+    regularPrice: 250,
+    discount: 0,
+    image: imageUrl + "cabin-001.jpg",
+    description:
+      "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
+  },
+  {
+    name: "002",
+    maxCapacity: 2,
+    regularPrice: 350,
+    discount: 25,
+    image: imageUrl + "cabin-002.jpg",
+    description:
+      "Escape to the serenity of nature and indulge in luxury in our cozy cabin 002. Perfect for couples, this cabin offers a secluded and intimate retreat in the heart of a picturesque forest. Inside, you will find warm and inviting interiors crafted from high-quality wood, a comfortable living area, a fireplace and a fully-equipped kitchen. The luxurious bedroom features a plush king-size bed and spa-like shower. Relax on the private deck with hot tub and take in the beauty of nature.",
+  },
+  {
+    name: "003",
+    maxCapacity: 4,
+    regularPrice: 300,
+    discount: 0,
+    image: imageUrl + "cabin-003.jpg",
+    description:
+      "Experience luxury family living in our medium-sized wooden cabin 003. Perfect for families of up to 4 people, this cabin offers a comfortable and inviting space with all modern amenities. Inside, you will find warm and inviting interiors crafted from high-quality wood, a comfortable living area, a fireplace, and a fully-equipped kitchen. The bedrooms feature plush beds and spa-like bathrooms. The cabin has a private deck with a hot tub and outdoor seating area, perfect for taking in the natural surroundings.",
+  },
+  {
+    name: "004",
+    maxCapacity: 4,
+    regularPrice: 500,
+    discount: 50,
+    image: imageUrl + "cabin-004.jpg",
+    description:
+      "Indulge in the ultimate luxury family vacation in this medium-sized cabin 004. Designed for families of up to 4, this cabin offers a sumptuous retreat for the discerning traveler. Inside, the cabin boasts of opulent interiors crafted from the finest quality wood, a comfortable living area, a fireplace, and a fully-equipped gourmet kitchen. The bedrooms are adorned with plush beds and spa-inspired en-suite bathrooms. Step outside to your private deck and soak in the natural surroundings while relaxing in your own hot tub.",
+  },
+  {
+    name: "005",
+    maxCapacity: 6,
+    regularPrice: 350,
+    discount: 0,
+    image: imageUrl + "cabin-005.jpg",
+    description:
+      "Enjoy a comfortable and cozy getaway with your group or family in our spacious cabin 005. Designed to accommodate up to 6 people, this cabin offers a secluded retreat in the heart of nature. Inside, the cabin features warm and inviting interiors crafted from quality wood, a living area with fireplace, and a fully-equipped kitchen. The bedrooms are comfortable and equipped with en-suite bathrooms. Step outside to your private deck and take in the natural surroundings while relaxing in your own hot tub.",
+  },
+  {
+    name: "006",
+    maxCapacity: 6,
+    regularPrice: 800,
+    discount: 100,
+    image: imageUrl + "cabin-006.jpg",
+    description:
+      "Experience the epitome of luxury with your group or family in our spacious wooden cabin 006. Designed to comfortably accommodate up to 6 people, this cabin offers a lavish retreat in the heart of nature. Inside, the cabin features opulent interiors crafted from premium wood, a grand living area with fireplace, and a fully-equipped gourmet kitchen. The bedrooms are adorned with plush beds and spa-like en-suite bathrooms. Step outside to your private deck and soak in the natural surroundings while relaxing in your own hot tub.",
+  },
+  {
+    name: "007",
+    maxCapacity: 8,
+    regularPrice: 600,
+    discount: 100,
+    image: imageUrl + "cabin-007.jpg",
+    description:
+      "Accommodate your large group or multiple families in the spacious and grand wooden cabin 007. Designed to comfortably fit up to 8 people, this cabin offers a secluded retreat in the heart of beautiful forests and mountains. Inside, the cabin features warm and inviting interiors crafted from quality wood, multiple living areas with fireplace, and a fully-equipped kitchen. The bedrooms are comfortable and equipped with en-suite bathrooms. The cabin has a private deck with a hot tub and outdoor seating area, perfect for taking in the natural surroundings.",
+  },
+  {
+    name: "008",
+    maxCapacity: 10,
+    regularPrice: 1400,
+    discount: 0,
+    image: imageUrl + "cabin-008.jpg",
+    description:
+      "Experience the epitome of luxury and grandeur with your large group or multiple families in our grand cabin 008. This cabin offers a lavish retreat that caters to all your needs and desires. The cabin features an opulent design and boasts of high-end finishes, intricate details and the finest quality wood throughout. Inside, the cabin features multiple grand living areas with fireplaces, a formal dining area, and a gourmet kitchen that is a chef's dream. The bedrooms are designed for ultimate comfort and luxury, with plush beds and en-suite spa-inspired bathrooms. Step outside and immerse yourself in the beauty of nature from your private deck, featuring a luxurious hot tub and ample seating areas for ultimate relaxation and enjoyment.",
+  },
+];
+const subtractDates = (dateStr1, dateStr2) =>
+  differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
+
